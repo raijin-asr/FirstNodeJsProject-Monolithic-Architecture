573d946d8f73df720775409098fa1d46472a517c
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2024-04-17 22:41:21 +0545

    app.get request and response

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 //NodeJs Practice
-console.log("Hello Ameer Rai")
+const express= require("express") //rote this two lines used in every nodejs
+const app= express() //invoke
+
+//const express= require("express")() //in single line
+
+//get part
+app.get('/',(req,res)=>{
+    res.send("Hello Ameer Rai")
+})
+
+app.listen(3000,()=>{
+    console.log("Server has started at port 3000 in terminal")
+})
