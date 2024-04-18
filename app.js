0d0410a2da969b0b140313e1da294c422645947b
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2024-04-18 16:29:48 +0545

    nodemon and --watch, rest and restful

--- app.js
+++ app.js
@@ -6,7 +6,28 @@ const app= express() //invoke
 
 //get part
 app.get('/',(req,res)=>{
+    // console.log(req)
     res.send("Hello Ameer Rai")
+})
+
+//rest
+app.get('/about',(req,res)=>{
+    res.send("About Ameer")
+})
+
+//restful
+app.get('/blog',(req,res)=>{
+    res.send("<h1>Blog with Ameer</h1>")
+})
+
+//restful
+app.get('/blog/create',(req,res)=>{
+    res.send("Create blog with Ameer")
+})
+
+//restful
+app.get('/blog/edit',(req,res)=>{
+    res.send("Edit blog with Ameer")
 })
 
 app.listen(3000,()=>{
