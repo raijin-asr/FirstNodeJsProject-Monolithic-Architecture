e5d5e0cfc2018202fbde92ca6bc8fff2682c6684
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2024-04-22 14:14:03 +0545

    create operation, post method

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 //NodeJs Practice
 const express= require("express") //rote this two lines used in every nodejs
+const { blogs } = require("./model/index") //auto imported after blogs typed and entered
 const app= express() //invoke
 
 //const express= require("express")() //in single line
 
 require("./model/index")
 app.set('view engine','ejs') //set ui render to ejs
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true})) //this shows posted data in console instead of undefined
 
 //get part
 app.get('/',(req,res)=>{
@@ -41,6 +45,32 @@ app.get('/blog/edit',(req,res)=>{
     // res.send("Edit blog with Ameer")
     res.render("editblog")
 })
+
+//post operation
+app.post('/blog', async(req,res)=>{ //for posting form data
+    // console.log(req.body) //for data in object in console, terminal
+    
+    //showing data of form and holding them
+    // const title= req.body.title
+    // const subtitle= req.body.subtitle
+    // const description= req.body.description
+    // console.log(title, subtitle, description)
+
+    //destructuring above 3 line js code to less line
+    const {title, subtitle,description}= req.body
+    console.log(title, subtitle, description)
+    
+    //accessing db table using db.blogs of index.js
+    await blogs.create({ //used await to bypass delay and added async in app.post
+        title: title, //in js, if two value is same in key value pair, write one title only
+        subTitle:subtitle,
+        description:description
+    })
+    // res.send("Blog created successfully")
+    res.redirect('/') //redirect
+}
+
+)
 
 app.listen(3000,()=>{
     console.log("Server has started at port 3000 in terminal")
